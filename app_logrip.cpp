0b228666df0241a0bd3ee52d15346eed1d56e688
Quanta Sciences <rhoetzlein@yahoo.com>
2025-07-12 16:12:19 -0400

    Update app_logrip.cpp

--- app_logrip.cpp
+++ app_logrip.cpp
@@ -1,13 +1,21 @@
-
 //--------------------------------------------------------------------------------
 // 
-// LogRip
+// LOGRIP
+// Defend against AI crawlers and bots with server log analysis
 // 
-// Copyright 2019-205 (c) Quanta Sciences, Rama Hoetzlein, ramakarl.com
-// * Derivative works may append the above copyright notice but should not remove or modify earlier notices.
+// Copyright 2024-2025 (c) Quanta Sciences, Rama Hoetzlein, https://ramakarl.com
 //
-// MIT License
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
+//--------------------------------------------------------------------------------
 
 #include "main.h"
 #include "timex.h"
